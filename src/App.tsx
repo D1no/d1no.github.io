67dcb86d2bb7bebec56e173499dfea7abbe097bb
Dino Scheidt <github@dino-scheidt.de>
2023-02-03 16:17:11 +0100

    💄 Set default browser theme color to black

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { x } from "@xstyled/emotion";
 
 function App() {
   return (
-    <div className="App">
+    <x.div className="App">
       <x.div whiteSpace="normal">
         <x.h1 text="xl">Hello!</x.h1>
       </x.div>
       <x.p>Website with xStyled</x.p>
-    </div>
+    </x.div>
   );
 }
 
