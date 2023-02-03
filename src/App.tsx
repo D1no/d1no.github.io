9b81d2161cb234690109282fa232be9c99a14f7b
Dino Scheidt <github@dino-scheidt.de>
2023-02-03 17:04:44 +0100

    🍱 Adding font Mona Sans and Hubot Sans

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <x.div className="App">
       <x.div whiteSpace="normal">
-        <x.h1 text="xl">Hello!</x.h1>
+        <x.h1 text="xl" fontWeight="600">
+          Hello!
+        </x.h1>
       </x.div>
       <x.p>Website with xStyled</x.p>
     </x.div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { defaultTheme, ThemeProvider, Preflight } from "@xstyled/emotion";
+import {
+  defaultTheme,
+  ThemeProvider,
+  Preflight,
+  createGlobalStyle,
+} from "@xstyled/emotion";
 
 const theme = {
   ...defaultTheme,
+  fonts: {
+    mono: defaultTheme.fonts.mono,
+    serif: defaultTheme.fonts.serif,
+    sans: "Mona Sans" + ", " + defaultTheme.fonts.sans,
+    header: "Hubot Sans" + ", " + defaultTheme.fonts.serif,
+  },
   // Customize your theme here
 };
+
+/**
+ * Sets the default font going forward.
+ */
+const ApplyFontStyle = createGlobalStyle`
+  body { 
+    font-family: ${theme.fonts.sans};
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: ${theme.fonts.header};
+  }
+`;
+
+console.dir(theme.texts);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Preflight />
+      <ApplyFontStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
